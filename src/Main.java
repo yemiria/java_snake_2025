34334a8043cb4219e72a0bc12d0d9eaaa8ada2e5
yemiria <AlwinBoros869@gmail.com>
2025-04-20 00:55:28 +0800

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+/**
+ * java_snake_2025
+ * java小游戏
+ * 2025/4/20
+ * @author Yemiria
+ * @author _
+ */
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
+
     }
 }
